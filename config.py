0e3ad1fbd61078fe8340a154d12fe224be89324c
sz-kristof <64156390+sz-kristof@users.noreply.github.com>
2025-05-05 12:49:12 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+from datetime import timedelta # <<< Import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # --- Add these lines for persistent sessions ---
+    SESSION_PERMANENT = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7) # Keep session cookie for 2 days
+    # --- End added lines ---
